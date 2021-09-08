541aa0315c23243f08eb393bcf5114840072e9ff
yasinarefin <yasinarefin13@gmail.com>
2021-09-08 22:52:55 +0600

    Update Combinatorics_nCr.cpp

--- Combinatorics_nCr.cpp
+++ Combinatorics_nCr.cpp
@@ -1,5 +1,6 @@
-const ll mod = 1e9 + 7;
-ll fac[1001];
+const ll mod = 998244353;
+const int MAX = 200000;
+ll fac[MAX+1];
 ll power(ll x, ll y){
    ll res = 1;
    x = x % mod;
@@ -11,18 +12,19 @@ ll power(ll x, ll y){
    }
    return res;
 }
- 
+
 ll modInverse(ll n) {
    return power(n, mod - 2);
 }
- 
+
 ll cal(ll n, ll r){
+   if(r > n) return 0;
    return (fac[n] * modInverse(fac[r]) % mod * modInverse(fac[n - r]) % mod) % mod;
 }
 void pre(){
    fac[0] = 1;
- 
-   for(int x=1;x<=1000;x++){
+
+   for(int x=1;x<=MAX;x++){
        fac[x] = fac[x-1]*x;
        fac[x] %= mod;
    }
